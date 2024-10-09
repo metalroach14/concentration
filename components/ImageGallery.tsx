425323367075205709b05e3de21c2a1c85559703
Toma Tomov <ttomov123@gmail.com>
2024-10-09 21:47:52 +0300

    remove unused imports

--- components/ImageGallery.tsx
+++ components/ImageGallery.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
 
 const ImageGallery = () => {
